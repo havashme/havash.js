7bf6bf7e928dc1df716b0f8490659ce5db78fea9
Havash <havash.me@gmail.com>
2024-03-31 18:21:51 +0330

    first init

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+ 
+
+
+function helloWorld(){
+    console.log("Hello World, My name is Havash.")
+}
+
+function jam(a,b){
+    return a + b
+}
+
+// test comment
+
+
+module.exports = {
+    helloWorld,
+    jam
+}
