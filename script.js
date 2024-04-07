6eab0e5d476b3aaa1b72f16adada4a32d5f51525
Havash <havash.me@gmail.com>
2024-04-07 22:46:50 +0330

    latest changes

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
+/**
+ * To push to git
+ * 1. open terminal
+ * 2. git add .
+ * 3. git commit -m "payam ro inja benevis"
+ * 4. git push origin main
+ */
 
 
- /**
-  * To push to git
-  * 1. open terminal
-  * 2. git add .
-  * 3. git commit -m "payam ro inja benevis"
-  * 4. git push origin main
-  */
-
-
-function helloWorld(){
+function helloWorld() {
     console.log("Hello World, My name is Havash.")
 }
 
-function jam(a,b){
+function jam(a, b) {
     return a + b
 }
 
-// 2122222222
+// its like setTimeout but simpler :) 😍😁
+function runAfter(millis, callback) {
+    setTimeout(callback, millis)
+}
+
+// its like setInterval but more simpler :)))
+function runEvery(millis, callback) {
+    setInterval(callback , millis)
+}
+
+
 
 
 module.exports = {
     helloWorld,
-    jam
+    jam,
+    runEvery,
+    runAfter
 }
